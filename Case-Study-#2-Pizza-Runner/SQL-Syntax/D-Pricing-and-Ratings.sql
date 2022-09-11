837451b577bfd8ae68d2f14fd651599ac067f3ed
Toludoyin <toludoyinshopein@gmail.com>
2022-09-11 14:44:15 +0100

    ratings-schema-left

--- Case-Study-#2-Pizza-Runner/SQL-Syntax/D-Pricing-and-Ratings.sql
+++ Case-Study-#2-Pizza-Runner/SQL-Syntax/D-Pricing-and-Ratings.sql
@@ -1,3 +1,7 @@
+------------------------
+-- D. Pricing and Rating
+------------------------
+
 -- 1. If a Meat Lovers pizza costs $12 and Vegetarian costs $10 and there were no charges for changes - how much money has Pizza Runner made so far if there are no delivery fees?
 with delivered_order as (
     select order_id, pizza_id,
