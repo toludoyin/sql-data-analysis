f4e2e47b5f0bb566c550f6b273ef40e5aa68fdc2
Toludoyin <toludoyinshopein@gmail.com>
2022-09-29 07:01:43 +0100

    in progress

--- Case-Study-#3-Foodie-Fi/SQL-Syntax/A-Customer-Journey.sql
+++ Case-Study-#3-Foodie-Fi/SQL-Syntax/A-Customer-Journey.sql
@@ -1,11 +1,14 @@
----------------------------------
--- CASE STUDY A. Customer Journey
----------------------------------
---Tools used: PostgreSQL
+/**
+-------------------------------
+ CASE STUDY A. Customer Journey
+-------------------------------
+Tools used: PostgreSQL
 
--- Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
+QUESTION
+Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
 
--- Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
+Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
+**/
 
 /**
 -------
