7097ed98904af4fed4d53bd7b08f05ed6d304e92
Toludoyin <toludoyinshopein@gmail.com>
2022-10-01 23:29:56 +0100

    analysis-in-progress

---
+++ Case-Study-#3-Foodie-Fi/SQL-Syntax/B-Data-Analysis-Questions.sql
@@ -0,0 +1,85 @@
+-- 1. How many customers has Foodie-Fi ever had?
+select
+count(distinct customer_id) as num_of_customers
+from foodie_fi.subscriptions;
+
+-- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+select
+date_trunc('month', start_date) as month_start,
+count(distinct customer_id) as num_of_cust
+from foodie_fi.subscriptions
+join foodie_fi.plans using(plan_id)
+where plan_name = 'trial'
+group by 1;
+
+-- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+select
+plan_name,
+count(*) as num_of_event
+from foodie_fi.subscriptions
+join foodie_fi.plans using(plan_id)
+where extract (year from start_date) >= 2020
+group by 1
+order by 2 desc;
+
+-- 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+select
+churn_cust as churn_count,
+total_cust,
+round((churn_cust/total_cust::numeric),1)*100 as churn_pertcg
+from (
+    select
+    count(distinct customer_id) as total_cust,
+    count(distinct customer_id) filter (where plan_name = 'churn')as churn_cust
+    from foodie_fi.subscriptions
+    join foodie_fi.plans using(plan_id)
+    ) as churn;
+
+-- 5. How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+select *,
+round((churn_after_trial/total_cust::numeric)* 100) as churn_after_trial_pretcg
+from (
+  select
+  count(distinct customer_id) as total_cust,
+  count(distinct customer_id) filter (where rn = 2 and plan_name ='churn') as churn_after_trial
+  from (
+    select *,
+    row_number() over(partition by customer_id order by start_date) as rn
+    from foodie_fi.subscriptions
+    join foodie_fi.plans using(plan_id)
+    order by 2
+  ) as churn_rn
+) as end_;
+
+-- 6. What is the number and percentage of customer plans after their initial free trial?
+
+-- 7. What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+with cust as (
+select *, row_number() over(partition by customer_id order by start_date) as rn
+from foodie_fi.subscriptions
+join foodie_fi.plans using(plan_id)
+where start_date::date <= '2020-12-31'::date
+order by 2
+),
+max_row as (
+select
+customer_id, max(rn) as max_row_num
+from cust
+group by 1
+)
+select *, round(no_of_cust::numeric/sum(no_of_cust) over()*100,1) as pertcg
+from (
+    select
+    plan_id,
+    count(*) as no_of_cust
+    from cust cc
+    join max_row mr on cc.customer_id = mr.customer_id
+    and cc.rn = mr.max_row_num
+    group by 1
+) as end_
+group by 1,2
+
+-- 8. How many customers have upgraded to an annual plan in 2020?
+-- 9. How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+-- 10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+-- 11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
