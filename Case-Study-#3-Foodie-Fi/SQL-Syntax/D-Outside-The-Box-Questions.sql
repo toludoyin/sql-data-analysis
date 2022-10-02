f833053de0d2541e6fcb5a07f590e4b03ea30e84
Toludoyin <toludoyinshopein@gmail.com>
2022-10-02 08:45:06 +0100

    add questions

---
+++ Case-Study-#3-Foodie-Fi/SQL-Syntax/D-Outside-The-Box-Questions.sql
@@ -0,0 +1,9 @@
+/**
+The following are open ended questions which might be asked during a technical interview for this case study - there are no right or wrong answers, but answers that make sense from both a technical and a business perspective make an amazing impression!
+
+How would you calculate the rate of growth for Foodie-Fi?
+What key metrics would you recommend Foodie-Fi management to track over time to assess performance of their overall business?
+What are some key customer journeys or experiences that you would analyse further to improve customer retention?
+If the Foodie-Fi team were to create an exit survey shown to customers who wish to cancel their subscription, what questions would you include in the survey?
+What business levers could the Foodie-Fi team use to reduce the customer churn rate? How would you validate the effectiveness of your ideas?
+**/
