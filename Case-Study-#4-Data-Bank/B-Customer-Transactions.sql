d0053ca7f3f34581b089eee6651067db97efbee9
Toludoyin <toludoyinshopein@gmail.com>
2022-10-31 06:57:38 +0100

    add case-study 4B- questions

---
+++ Case-Study-#4-Data-Bank/B-Customer-Transactions.sql
@@ -0,0 +1,7 @@
+-- 1. What is the unique count and total amount for each transaction type?
+
+
+-- 2. What is the average total historical deposit counts and amounts for all customers?
+-- 3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+-- 4. What is the closing balance for each customer at the end of the month?
+-- 5. What is the percentage of customers who increase their closing balance by more than 5%?
