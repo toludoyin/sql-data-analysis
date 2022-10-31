6a5ad84d239a4ffbacb233a8268d86d07d0c9cdd
Toludoyin <toludoyinshopein@gmail.com>
2022-10-31 06:50:54 +0100

    complete customer-nodes-exploration

--- Case-Study-#4-Data-Bank/A-Customer-Nodes-Exploration.sql
+++ Case-Study-#4-Data-Bank/A-Customer-Nodes-Exploration.sql
@@ -20,7 +20,27 @@ group by 1,2
 order by 1;
 
 -- 4. How many days on average are customers reallocated to a different node?
+select round(avg(end_date - start_date),1) as avg_date
+from (
+    select
+    customer_id, start_date,
+    case when end_date > current_date then '2020-12-31'::date else end_date end as end_date
+    from data_bank.customer_nodes
+  )avg_date;
 
 -- 5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
-
-
+with nodes as (
+    select *, end_date - start_date as node_days
+    from (
+        select
+        region_id, start_date,
+        case when end_date > current_date then '2020-12-31'::date else end_date end as end_date
+        from data_bank.customer_nodes
+    )tmp
+ )
+select
+region_id, percentile_cont(0.5) within group (order by node_days) as median,
+percentile_cont(0.8) within group (order by node_days) as q80,
+round(percentile_cont(0.95) within group (order by node_days)::numeric,1) as q95
+from nodes
+group by region_id;
