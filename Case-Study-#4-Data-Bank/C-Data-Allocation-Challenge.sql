d20f50ad5758cea8ebb671406958284a8477b619
Toludoyin <toludoyinshopein@gmail.com>
2022-11-06 04:08:21 +0100

    data-allocation in progress

---
+++ Case-Study-#4-Data-Bank/C-Data-Allocation-Challenge.sql
@@ -0,0 +1,12 @@
+-- To test out a few different hypotheses - the Data Bank team wants to run an experiment where different groups of customers would be allocated data using 3 different options:
+
+-- Option 1: data is allocated based off the amount of money at the end of the previous month
+-- Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
+-- Option 3: data is updated real-time
+-- For this multi-part challenge question - you have been requested to generate the following data elements to help the Data Bank team estimate how much data will need to be provisioned for each option:
+
+-- running customer balance column that includes the impact each transaction
+-- customer balance at the end of each month
+-- minimum, average and maximum values of the running balance for each customer
+-- Using all of the data available - how much data would have been required for each option on a monthly basis?
+
