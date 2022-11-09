dd7a92945ed07fe2fbff02deda44b8a1ea0afc4c
Toludoyin <toludoyinshopein@gmail.com>
2022-11-09 11:33:21 +0100

    modify 4C-option2

--- Case-Study-#4-Data-Bank/C-Data-Allocation-Challenge.sql
+++ Case-Study-#4-Data-Bank/C-Data-Allocation-Challenge.sql
@@ -45,55 +45,50 @@ order by 1;
 
 -- Option 2
 with txn_deposit as (
-select *,
-  case when txn_type = 'deposit'
-  then txn_amount else -1 * txn_amount end as txn_group
-from data_bank.customer_transactions
+    select *, case when txn_type = 'deposit'
+    then txn_amount else -1 * txn_amount end as txn_group
+    from data_bank.customer_transactions
 ),
 date_series as (
- select customer_id,
-  generate_series(first_date, last_date, '1 day') as date_series
-  from (
-    select customer_id,
-    max(txn_date) as last_date, min(txn_date) as first_date
-from txn_deposit
-    group by 1
-    ) min_max_series
-  ),
-  customer_balance as (
-  select *, sum(txn_group) over (partition by customer_id order by date_series) as txn_sum
+    select customer_id, generate_series(first_date, last_date, '1 day') as date_series
     from (
-    select ds.customer_id, date_series, txn_group,
-  count(txn_group) over (partition by ds.customer_id order by date_series) as txn_count
-    from date_series ds
-    left join txn_deposit td on ds.customer_id = td.customer_id
-    and ds.date_series = td.txn_date
-      order by ds.customer_id, date_series
-    --group by 1,2,3
+        select customer_id, max(txn_date) as last_date,
+        min(txn_date) as first_date
+        from txn_deposit
+        group by 1
+    ) min_max_series
+),
+customer_balance as (
+    select *, sum(txn_group) over (partition by customer_id order by date_series) as txn_sum
+    from (
+        select ds.customer_id, date_series, txn_group,
+        count(txn_group) over (partition by ds.customer_id order by date_series) as txn_count
+        from date_series ds
+        left join txn_deposit td on ds.customer_id = td.customer_id
+        and ds.date_series = td.txn_date
+        order by ds.customer_id, date_series
     ) as cust_bal_count
-    ),
-    customer_data as (
-     select customer_id,
-    date_series, case when txn_row_no < 30 then NULL
+),
+customer_data as (
+    select customer_id, date_series, case when txn_row_no < 30 then NULL
     when avg_last_30_days < 0 then 0
     else avg_last_30_days end as data_store
     from (
-    select *,
-    avg(txn_sum) over(partition by date_series rows between 30 preceding and current row) as avg_last_30_days,
-    row_number() over(partition by customer_id order by date_series) as txn_row_no
-    from customer_balance
-    order by 1
-) as last_30_days
-      )
-      select
-      month, round(sum(data_allocation),1) as total_allocation from (
-        select customer_id,
-      date_trunc('month', date_series) as month,
-      max(data_store) as data_allocation
-      from customer_data
-   group by customer_id,month
-        ) as allocated_data
-        group by 1
+        select *,
+        avg(txn_sum) over(partition by customer_id order by date_series rows between 30 preceding and current row) as avg_last_30_days,
+        row_number() over(partition by customer_id order by date_series) as txn_row_no
+        from customer_balance
         order by 1
-
-
+    ) as last_30_days
+)
+select
+month, round(sum(data_allocation),1) as total_allocation
+from (
+    select customer_id,
+    date_trunc('month', date_series) as month,
+    max(data_store) as data_allocation
+    from customer_data
+    group by customer_id,month
+) as allocated_data
+group by 1
+order by 1
