602f7d98b5e0a0570f649891ed1a513fa86aa6ad
Toludoyin <toludoyinshopein@gmail.com>
2022-11-18 00:10:58 +0100

    before-after in progress

---
+++ Case-Study-#5-Data-Mart/3-Before-After-Analysis.sql
@@ -0,0 +1,14 @@
+/*
+This technique is usually used when we inspect an important event and want to inspect the impact before and after a certain point in time.
+
+Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging changes came into effect.
+
+We would include all week_date values for 2020-06-15 as the start of the period after the change and the previous week_date values would be before
+
+Using this analysis approach - answer the following questions:
+
+What is the total sales for the 4 weeks before and after 2020-06-15? What is the growth or reduction rate in actual values and percentage of sales?
+What about the entire 12 weeks before and after?
+How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?
+*/
+
