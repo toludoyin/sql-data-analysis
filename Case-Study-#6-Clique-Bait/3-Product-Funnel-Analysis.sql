45b986b2932c8399c809543a97ae15bd10ca19a0
Toludoyin <toludoyinshopein@gmail.com>
2022-11-25 20:16:00 +0100

    add question

---
+++ Case-Study-#6-Clique-Bait/3-Product-Funnel-Analysis.sql
@@ -0,0 +1,17 @@
+/**
+Using a single SQL query - create a new output table which has the following details:
+
+How many times was each product viewed?
+How many times was each product added to cart?
+How many times was each product added to a cart but not purchased (abandoned)?
+How many times was each product purchased?
+Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
+
+Use your 2 new output tables - answer the following questions:
+
+Which product had the most views, cart adds and purchases?
+Which product was most likely to be abandoned?
+Which product had the highest view to purchase percentage?
+What is the average conversion rate from view to cart add?
+What is the average conversion rate from cart add to purchase?
+**/
