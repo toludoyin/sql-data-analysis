0c2673e047c0f62cab644ec27c08490b6f3570ef
Toludoyin <toludoyinshopein@gmail.com>
2022-11-25 20:15:37 +0100

    add question

---
+++ Case-Study-#6-Clique-Bait/2-Digital-Analysis.sql
@@ -0,0 +1,13 @@
+/**
+Using the available datasets - answer the following questions using a single query for each one:
+
+How many users are there?
+How many cookies does each user have on average?
+What is the unique number of visits by all users per month?
+What is the number of events for each event type?
+What is the percentage of visits which have a purchase event?
+What is the percentage of visits which view the checkout page but do not have a purchase event?
+What are the top 3 pages by number of views?
+What is the number of views and cart adds for each product category?
+What are the top 3 products by purchases?
+**/
