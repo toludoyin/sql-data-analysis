4d2d49056c5705a879fe1d0d3fb9bed0e01a2028
Toludoyin <toludoyinshopein@gmail.com>
2022-12-11 23:15:05 +0100

    1a-down

--- Case-Study-#6-Clique-Bait/3-Product-Funnel-Analysis.sql
+++ Case-Study-#6-Clique-Bait/3-Product-Funnel-Analysis.sql
@@ -6,7 +6,17 @@ How many times was each product added to cart?
 How many times was each product added to a cart but not purchased (abandoned)?
 How many times was each product purchased?
 Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
+**/
 
+select page_name, count(*) as count
+from clique_bait.events
+join clique_bait.event_identifier using(event_type)
+left join clique_bait.page_hierarchy using(page_id)
+where event_name = 'Page View'
+group by 1 desc
+limit 10;
+
+/**
 Use your 2 new output tables - answer the following questions:
 
 Which product had the most views, cart adds and purchases?
