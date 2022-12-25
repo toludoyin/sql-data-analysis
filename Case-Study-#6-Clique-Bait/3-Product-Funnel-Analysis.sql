a76dfc04d6fab7aa8bfe3cc4ccf84a17aeea9aff
Toludoyin <toludoyinshopein@gmail.com>
2022-12-25 06:16:10 +0100

    product-funnel-analysis

--- Case-Study-#6-Clique-Bait/3-Product-Funnel-Analysis.sql
+++ Case-Study-#6-Clique-Bait/3-Product-Funnel-Analysis.sql
@@ -37,4 +37,41 @@ Which product was most likely to be abandoned?
 Which product had the highest view to purchase percentage?
 What is the average conversion rate from view to cart add?
 What is the average conversion rate from cart add to purchase?
+
+Answers:
+Lobster: had the most veiws
 **/
+
+with purchase as (
+  select visit_id
+  from clique_bait.events e
+  where page_id = 13
+),
+abandoned as (
+  select visit_id
+  from clique_bait.events e
+  where page_id = 12 and page_id <> 13
+  ),
+  summary as (
+    select
+    product_id, page_name,
+    count(visit_id) filter(where event_name = 'Page View') as total_page_views,
+    count(visit_id) filter(where event_name = 'Add to Cart') as total_cart_adds,
+    count(case when visit_id in (select visit_id from purchase) and event_type not in (1,4,5) then 1 end) as product_purchase,
+ count(case when event_name = 'Add to Cart' and visit_id in (select visit_id from abandoned) then 1 end) as abandoned_add_to_cart                           from clique_bait.events e
+  left join clique_bait.page_hierarchy ph using(page_id)
+  left join clique_bait.event_identifier using(event_type)
+  where product_id is not null
+  group by 1,2
+    )
+    select
+    round(avg(cart_add_to_views),2) as avg_views_to_cart,
+    round(avg(purchase_from_add_to_cart),2) as avg_views_to_cart
+    from (
+      select *,
+    round((total_cart_adds / total_page_views::numeric)*100,2) as cart_add_to_views,
+    round((product_purchase / total_page_views::numeric)*100,2) as purchase_to_views,
+    (product_purchase / total_cart_adds::numeric)*100 as purchase_from_add_to_cart
+    from summary
+    order by 7 desc
+      ) as tmp
