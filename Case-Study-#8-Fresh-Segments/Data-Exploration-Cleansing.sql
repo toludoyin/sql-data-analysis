5c562d79e710c3a858a6b5139569ef4f86dbffb2
Toludoyin <toludoyinshopein@gmail.com>
2023-03-21 01:40:51 +0100

    Q1

--- Case-Study-#8-Fresh-Segments/Data-Exploration-Cleansing.sql
+++ Case-Study-#8-Fresh-Segments/Data-Exploration-Cleansing.sql
@@ -1,4 +1,7 @@
 -- Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
+ALTER TABLE fresh_segments.interest_metrics
+ALTER COLUMN month_year TYPE DATE
+USING month_year::DATE
 
 -- What is count of records in the fresh_segments.interest_metrics for each month_year value sorted in chronological order (earliest to latest) with the null values appearing first?
 -- What do you think we should do with these null values in the fresh_segments.interest_metrics
