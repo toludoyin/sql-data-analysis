fd6c0f2b52cd01bf71be45ea9b17b46b8a63765e
Toludoyin <toludoyinshopein@gmail.com>
2023-03-23 23:28:02 +0100

    modify-query-row6

--- Case-Study-#8-Fresh-Segments/Data-Exploration-Cleansing.sql
+++ Case-Study-#8-Fresh-Segments/Data-Exploration-Cleansing.sql
@@ -2,7 +2,7 @@
 -- DATA EXPLORATION AND CLEANING
 --------------------------------
 
--- Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
+--1. Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
 ALTER TABLE fresh_segments.interest_metrics
 ALTER COLUMN month_year TYPE DATE
 USING TO_DATE(month_year, 'MM-YYYY');
@@ -10,7 +10,7 @@ USING TO_DATE(month_year, 'MM-YYYY');
 SELECT * FROM fresh_segments.interest_metrics
 LIMIT 5;
 
--- What is count of records in the fresh_segments.interest_metrics for each month_year value sorted in chronological order (earliest to latest) with the null values appearing first?
+--2. What is count of records in the fresh_segments.interest_metrics for each month_year value sorted in chronological order (earliest to latest) with the null values appearing first?
 SELECT
     DATE_TRUNC('MONTH', month_year) AS month_year,
     COUNT(*) AS record_count
@@ -18,10 +18,10 @@ FROM fresh_segments.interest_metrics
 GROUP BY 1
 ORDER BY month_year NULLS FIRST;
 
--- What do you think we should do with these null values in the fresh_segments.interest_metrics
+--3. What do you think we should do with these null values in the fresh_segments.interest_metrics
 -- **Non-coding question**
 
--- How many interest_id values exist in the fresh_segments.interest_metrics table but not in the fresh_segments.interest_map table? What about the other way around?
+--4. How many interest_id values exist in the fresh_segments.interest_metrics table but not in the fresh_segments.interest_map table? What about the other way around?
 SELECT
     COUNT(DISTINCT interest_id) AS num_of_interest_id
 FROM fresh_segments.interest_metrics
@@ -40,12 +40,12 @@ WHERE id NOT IN (
     WHERE interest_id IS NOT NULL
  );
 
--- Summarise the id values in the fresh_segments.interest_map by its total record count in this table
+--5. Summarise the id values in the fresh_segments.interest_map by its total record count in this table
 SELECT
 	COUNT(*) AS total_records
 FROM fresh_segments.interest_map;
 
--- What sort of table join should we perform for our analysis and why? Check your logic by checking the rows where interest_id = 21246 in your joined output and include all columns from fresh_segments.interest_metrics and all columns from fresh_segments.interest_map except from the id column.
+--6. What sort of table join should we perform for our analysis and why? Check your logic by checking the rows where interest_id = 21246 in your joined output and include all columns from fresh_segments.interest_metrics and all columns from fresh_segments.interest_map except from the id column.
 SELECT
     DISTINCT ime.interest_id::INT,
     interest_name,
@@ -61,9 +61,10 @@ SELECT
     percentile_ranking
 FROM fresh_segments.interest_map AS ima
 JOIN fresh_segments.interest_metrics AS ime ON ima.id = ime.interest_id::INT
-WHERE interest_id = '21246';
+WHERE interest_id = '21246'
+AND month_year IS NOT NULL;
 
--- Are there any records in your joined table where the month_year value is before the created_at value from the fresh_segments.interest_map table? Do you think these values are valid and why?
+--7. Are there any records in your joined table where the month_year value is before the created_at value from the fresh_segments.interest_map table? Do you think these values are valid and why?
 SELECT COUNT(*) AS "month_year < created_at"
 FROM fresh_segments.interest_map AS ima
 JOIN fresh_segments.interest_metrics AS ime ON ima.id = ime.interest_id::INT
