8878153dd7cb82fd62e902542532cc81658c35cc
Toludoyin <toludoyinshopein@gmail.com>
2023-03-23 13:09:35 +0100

    update-lowercase-key-to-uppercase

--- Case-Study-#2-Pizza-Runner/SQL-Syntax/A.-Pizza-Metrics.sql
+++ Case-Study-#2-Pizza-Runner/SQL-Syntax/A.-Pizza-Metrics.sql
@@ -4,127 +4,126 @@
 --Tools used: PostgreSQL
 
 -- 1. How many pizzas were ordered?
-select
-count(pizza_id) as total_pizzas_ordered
-from pizza_runner.customer_orders;
+SELECT
+    COUNT(pizza_id) AS total_pizzas_ordered
+FROM pizza_runner.customer_orders;
 
 -- 2. How many unique customer orders were made?
-select
-count(distinct order_id) as unique_customers
-from pizza_runner.customer_orders;
+SELECT
+    COUNT(DISTINCT order_id) AS unique_customers
+FROM pizza_runner.customer_orders;
 
 -- 3. How many successful orders were delivered by each runner?
-select runner_id,
-count(order_id) as  num_of_orders_delivered
-from (
-    select *,
-    case when Cancellation = 'null'
-    or Cancellation is null
-    or Cancellation = '' then 0 else 1 end as cancellation2
-    from pizza_runner.runner_orders
-) as successful_orders
-where cancellation2 = 0
-group by 1
-order by 1;
+SELECT runner_id,
+   COUNT(order_id) AS num_of_orders_delivered
+FROM (
+    SELECT *,
+        CASE WHEN Cancellation = 'null'
+        OR Cancellation IS NULL
+        OR Cancellation = '' THEN 0 ELSE 1 END AS cancellation2
+    FROM pizza_runner.runner_orders
+) AS successful_orders
+WHERE cancellation2 = 0
+GROUP BY 1
+ORDER BY 1;
 
 -- 4. How many of each type of pizza was delivered?
-with pizza_types as (
-    select *,
-    case when Cancellation = 'null'
-    or Cancellation is null
-    or Cancellation = '' then 0 else 1 end as cancellation2
-    from pizza_runner.runner_orders ro
-    join pizza_runner.customer_orders co using(order_id)
-left join pizza_runner.pizza_names pn on co.pizza_id = pn.pizza_id
+WITH pizza_types AS (
+    SELECT *,
+        CASE WHEN Cancellation = 'null'
+        OR Cancellation IS NULL
+        OR Cancellation = '' THEN 0 ELSE 1 END AS cancellation2
+    FROM pizza_runner.runner_orders ro
+    JOIN pizza_runner.customer_orders co USING(order_id)
+    LEFT JOIN pizza_runner.pizza_names pn ON co.pizza_id = pn.pizza_id
 )
-select
-pizza_name,
-count(order_id) as num_of_orders
-from pizza_types
-where cancellation2 = 0
-group by 1
-order by 1;
+SELECT
+    pizza_name,
+    COUNT(order_id) AS num_of_orders
+FROM pizza_types
+WHERE cancellation2 = 0
+GROUP BY 1
+ORDER BY 1;
 
 -- 5.How many Vegetarian and Meatlovers were ordered by each customer?
-select
-customer_id,
-coalesce(count(case when pizza_name='Meatlovers' then order_id end),0) as Meatlovers,
-coalesce(count(case when pizza_name='Vegetarian' then order_id end),0) as Vegetarian
-from pizza_runner.runner_orders ro
-join pizza_runner.customer_orders co using(order_id)
-left join pizza_runner.pizza_names pn on co.pizza_id = pn.pizza_id
-group by 1
-order by 1;
+SELECT
+    customer_id,
+    COALESCE(COUNT(CASE WHEN pizza_name='Meatlovers' THEN order_id END),0) AS Meatlovers,
+    coalesce(count(CASE WHEN pizza_name='Vegetarian' THEN order_id END),0) AS Vegetarian
+FROM pizza_runner.runner_orders ro
+JOIN pizza_runner.customer_orders co USING(order_id)
+LEFT JOIN pizza_runner.pizza_names pn ON co.pizza_id = pn.pizza_id
+GROUP BY 1
+ORDER BY 1;
 
 -- 6. What was the maximum number of pizzas delivered in a single order?
-select order_id,
-count(pizza_id) as num_of_pizzas_delivered
-from pizza_runner.customer_orders
-group by 1
-order by 2 desc;
+SELECT
+    order_id,
+    COUNT(pizza_id) AS num_of_pizzas_delivered
+FROM pizza_runner.customer_orders
+GROUP BY 1
+ORDER BY 2 DESC;
 
 -- 7 and 8
-with cleaned_data as (
-    select *,
-    case when exclusions in ('null', '') or exclusions is null then '0'
-    else exclusions end as exclusion,
-    case when extras in ('null', '') or extras is null then '0'
-    else extras end as extra
-    from pizza_runner.customer_orders
-    where order_id in
-    (
-        select distinct order_id from
-        (
-            select *,
-            case when Cancellation is null
-            or Cancellation in('null', '') then 0 else 1 end as cancellation2
-            from pizza_runner.runner_orders
-        ) as successful_pizza
-        where cancellation2 = 0
+WITH cleaned_data AS (
+    SELECT *,
+        CASE WHEN exclusions IN ('null', '') OR exclusions IS NULL THEN '0'
+        ELSE exclusions END AS exclusion,
+        CASE WHEN extras IN ('null', '') OR extras IS NULL THEN '0'
+        ELSE extras END AS extra
+    FROM pizza_runner.customer_orders
+    WHERE order_id IN (
+        SELECT DISTINCT order_id FROM (
+            SELECT *,
+                CASE WHEN Cancellation IS NULL OR Cancellation in('null', '') THEN 0 ELSE 1 END AS cancellation2
+            FROM pizza_runner.runner_orders
+        ) AS successful_pizza
+        WHERE cancellation2 = 0
     )
 ),
-create_change_col as (
-    select *,
-    case when exclusion = '0' and extra = '0' then 0 else 1 end as change
-    from cleaned_data
+create_change_col AS (
+   SELECT *,
+        CASE WHEN exclusion = '0' AND extra = '0' THEN 0 ELSE 1 END AS change
+    FROM cleaned_data
 ),
-made_changes as (
-    select
-    customer_id,
-    count(order_id) filter (where change::int = 0) as pizza_no_changes,
-    count(order_id) filter (where change::int > 0) as pizza_changes
-    from create_change_col
-    group by 1
-    order by 1
+made_changes AS (
+    SELECT
+        customer_id,
+        COUNT(order_id) FILTER (WHERE change::INT = 0) AS pizza_no_changes,
+        COUNT(order_id) FILTER (WHERE change::INT > 0) AS pizza_changes
+    FROM create_change_col
+    GROUP BY 1
+    ORDER BY 1
 ),   -- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-made_both_change as (
-    select customer_id,
-    count(order_id) filter (where exclu_change::int > 0 and extra_change::int > 0) as exclusion_extra
-    from (
-        select *,
-        case when exclusion = '0' then 0 else 1 end as exclu_change,
-        case when extra = '0' then 0 else 1 end as extra_change
-        from create_change_col
-        ) as made_both_changes
-    group by 1
-    order by 1
+made_both_change AS (
+    SELECT
+        customer_id,
+        COUNT(order_id) FILTER (WHERE exclu_change::INT > 0 AND extra_change::INT > 0) AS exclusion_extra
+    FROM (
+        SELECT *,
+            CASE WHEN exclusion = '0' THEN 0 ELSE 1 END AS exclu_change,
+            CASE WHEN extra = '0' THEN 0 ELSE 1 END AS extra_change
+        FROM create_change_col
+        ) AS made_both_changes
+    GROUP BY 1
+    ORDER BY 1
 )       --8. How many pizzas were delivered that had both exclusions and extras?
-select *
--- from made_changes       -- 7.
-from made_both_change;      -- 8.
+SELECT *
+-- FROM made_changes       -- 7.
+FROM made_both_change;      -- 8.
 
 -- 9. What was the total volume of pizzas ordered for each hour of the day?
-select
-extract(hour from order_time) as hour_of_day,
-count(order_id) as num_of_order
-from pizza_runner.customer_orders
-group by 1;
+SELECT
+    EXTRACT(hour FROM order_time) AS hour_of_day,
+    COUNT(order_id) AS num_of_order
+FROM pizza_runner.customer_orders
+GROUP BY 1;
 
 -- 10. What was the volume of orders for each day of the week?
-select
-to_char(order_time, 'Day') as day_of_week,
-extract(dow from order_time) as week_day,
-count(order_id) as num_of_orders
-from pizza_runner.customer_orders
-group by 1,2
-order by 3 desc;
+SELECT
+    TO_CHAR(order_time, 'Day') AS day_of_week,
+    EXTRACT(dow FROM order_time) AS week_day,
+    COUNT(order_id) AS num_of_orders
+FROM pizza_runner.customer_orders
+GROUP BY 1,2
+ORDER BY 3 DESC;
