136fecb17a87b5fefd1a39b7c6e788cf27c51310
Toludoyin <toludoyinshopein@gmail.com>
2023-03-24 02:14:25 +0100

    toggle-comment

--- Case-Study-#2-Pizza-Runner/SQL-Syntax/E-Bonus-Question.sql
+++ Case-Study-#2-Pizza-Runner/SQL-Syntax/E-Bonus-Question.sql
@@ -1,4 +1,6 @@
-/*If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
+/*If Danny wants to expand his range of pizzas - how would this impact the
+existing data design? Write an INSERT statement to demonstrate what would
+happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
 */
 
 INSERT INTO pizza_runner.pizza_names
