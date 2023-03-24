65c4645eb804edb92997fc8e027a64365bfbd193
Toludoyin <toludoyinshopein@gmail.com>
2023-03-24 02:17:33 +0100

    modify-keyword-case

--- Case-Study-#2-Pizza-Runner/SQL-Syntax/C-Ingredient-Optimisation.sql
+++ Case-Study-#2-Pizza-Runner/SQL-Syntax/C-Ingredient-Optimisation.sql
@@ -124,7 +124,9 @@ LEFT JOIN exclusions_toppings t USING(row_index)
 JOIN pizza_runner.pizza_names USING(pizza_id)
 WHERE pizza_name = 'Meatlovers';
 
-/**5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+/**5. Generate an alphabetically ordered comma separated ingredient list for
+each pizza order from the customer_orders table and add a 2x in front of any
+relevant ingredients
 For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
 **/
 WITH row_indexx AS (
