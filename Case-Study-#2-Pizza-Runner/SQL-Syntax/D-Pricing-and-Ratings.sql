be98776c02fb1264d88c0dc45dc2dda61aac2793
Toludoyin <toludoyinshopein@gmail.com>
2023-03-24 02:17:54 +0100

    toggle-comment

--- Case-Study-#2-Pizza-Runner/SQL-Syntax/D-Pricing-and-Ratings.sql
+++ Case-Study-#2-Pizza-Runner/SQL-Syntax/D-Pricing-and-Ratings.sql
@@ -58,7 +58,10 @@ FROM (
 ) AS price_and_charges
 GROUP BY 1;
 
-/*3. The Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, how would you design an additional table for this new dataset - generate a schema for this new table and insert your own data for ratings for each successful customer order between 1 to 5.
+/*3. The Pizza Runner team now wants to add an additional ratings system that
+allows customers to rate their runner, how would you design an additional table
+for this new dataset - generate a schema for this new table and insert your own
+data for ratings for each successful customer order between 1 to 5.
 NOTE: full schema available in new_schema.sql file
 */
 DROP TABLE IF EXISTS ratings;
@@ -82,7 +85,8 @@ VALUES
 -- query
 SELECT * FROM pizza_runner.ratings
 
-/*4. Using your newly generated table - can you join all of the information together to form a table which has the following information for successful deliveries?
+/*4. Using your newly generated table - can you join all of the information
+together to form a table which has the following information for successful deliveries?
 * customer_id
 * order_id
 * runner_id
