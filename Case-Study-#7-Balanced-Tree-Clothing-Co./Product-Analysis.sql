30a9aa62ea784f7f359dd9e1504149be8c354baa
Toludoyin <toludoyinshopein@gmail.com>
2023-07-13 21:34:04 +0100

    add question

---
+++ Case-Study-#7-Balanced-Tree-Clothing-Co./Product-Analysis.sql
@@ -0,0 +1,10 @@
+-- What are the top 3 products by total revenue before discount?
+-- What is the total quantity, revenue and discount for each segment?
+-- What is the top selling product for each segment?
+-- What is the total quantity, revenue and discount for each category?
+-- What is the top selling product for each category?
+-- What is the percentage split of revenue by product for each segment?
+-- What is the percentage split of revenue by segment for each category?
+-- What is the percentage split of total revenue by category?
+-- What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+-- What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
